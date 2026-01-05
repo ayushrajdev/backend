99dbd5048fd3209b235d8d0b16f69322bacaeda8
ayushrajdev <ayushrajdev@github.com>
2026-01-05 14:39:37 +0530

    added upload feature

--- http_server_using_net_module/server.js
+++ http_server_using_net_module/server.js
@@ -2,30 +2,57 @@ import net from "node:net";
 import fs, { open } from "node:fs/promises";
 
 const server = net.createServer(async (socket) => {
-  const readhandle = await open("./file.txt");
-  const readStream = await readhandle.createReadStream({ highWaterMark: 1000 });
 
+    const writeHandle = await open("output.txt","w+")
+    const writeStream = await writeHandle.createWriteStream()
+
+//   const fileHandle = await open(
+//     "C:\\Users\\ayush\\Downloads\\The Social Network (2010) Dual Audio {Hindi-English} 1080p.mkv"
+//   );
+//   const readStream = await fileHandle.createReadStream({
+//     highWaterMark: 1000,
+//   });
+//   const { size } = await fileHandle.stat();
   socket.on("data", async (chunk) => {
     // when the browser send the request data event fires and then browser writes the request as a header.
-    console.log(chunk.toString());
-    socket.write("HTTP/1.1\n");
-    socket.write("Access-Control-Allow-Origin: *\n");
-    socket.write("hello: world");
-    socket.write("\n\n");
-    await readStream.pipe(socket);
+    // console.log(chunk.toString());
+    // socket.write("HTTP/1.1\n");
+    // socket.write("Access-Control-Allow-Origin: *\n");
+    // socket.write(`Content-Length: ${size}\n`);
+    // socket.write(`Content-Type: text/plain \n`);
+    // socket.write(`Content-Type: image/jpeg \n`);
+    // socket.write(`Content-Disposition:attachment;   \n`);
+    // socket.write("hello: world");
+    // socket.write("\n\n");
+    // await readStream.pipe(socket);
+    // console.log(chunk.toString())
+
+    if (/WebKitFormBoundary.+--/.test(chunk.toString())) {
+        socket.end("got the data")
+    }
+    writeStream.write(chunk)
   });
 
   socket.on("error", (e) => {
     console.log("socket error  ", e);
   });
 
-  readStream.on("end", () => {
-    socket.end("wow");
-  });
+//   readStream.on("end", () => {
+//     socket.end("wow");
+//     fileHandle.close()
+//   });
 
-  readStream.on("error", (error) => {
-    socket.end(error);
-  });
+//   readStream.on("pause", () => {
+//     console.log("paused by the browser");
+//   });
+
+//   readStream.on("resume", () => {
+//     console.log("resuemd by the browser");
+//   });
+
+//   readStream.on("error", (error) => {
+//     socket.end(error);
+//   });
 });
 
 server.listen(3000, () => {
